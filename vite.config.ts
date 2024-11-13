1caca8abce37cba649acd35b8bc91bdb232d26d1
Chudinovskikh, Dmitry <dchudino3@gatech.edu>
2024-11-13 14:07:43 -0500

    pop7

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: './',
     plugins: [
         svgr({
             svgrOptions: {
