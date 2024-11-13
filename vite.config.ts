6bcaf2dec39c34ba986ac5d58d04f379d7c2c42b
Chudinovskikh, Dmitry <dchudino3@gatech.edu>
2024-11-13 13:30:01 -0500

    pop6

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/healthspectrum/',
+    base: '/',
     plugins: [
         svgr({
             svgrOptions: {
