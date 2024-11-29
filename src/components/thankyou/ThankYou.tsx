17217e8de997cb37b60307a6f51cc9a3b5e90834
Chudinovskikh, Dmitry <dchudino3@gatech.edu>
2024-11-29 14:43:14 -0500

    cosmetic

--- src/components/thankyou/ThankYou.tsx
+++ src/components/thankyou/ThankYou.tsx
@@ -2,7 +2,7 @@ export default function ThankYou() {
     return (
         <div>
             <h1>Thank You!</h1>
-            <p>Your improvement plan has been sent to your email.</p>
+            <p>Your improvement plan has been sent to your email . </p>
         </div>
     );
 }
