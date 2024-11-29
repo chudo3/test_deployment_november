635c05eddb1e27a2901deebabb077f05a837a8f7
Chudinovskikh, Dmitry <dchudino3@gatech.edu>
2024-11-29 15:10:13 -0500

    thank you changes

--- src/components/thankyou/ThankYou.tsx
+++ src/components/thankyou/ThankYou.tsx
@@ -1,8 +1,19 @@
 export default function ThankYou() {
     return (
-        <div>
-            <h1>Thank You!</h1>
-            <p>Your improvement plan has been sent to your email . </p>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh", // Full viewport height
+                textAlign: "center", // Center the text
+            }}
+        >
+            <h1 style={{ fontSize: "3rem", margin: "0.5rem" }}>Thank You!</h1>
+            <p style={{ fontSize: "1.5rem", margin: "0.5rem" }}>
+                Your improvement plan has been sent to your email.
+            </p>
         </div>
     );
 }
